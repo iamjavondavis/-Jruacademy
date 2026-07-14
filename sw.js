57a001c6cd71cdbe79f5b43d3e71783852db4682
iamjavondavis <javon.davis21@gmail.com>
2026-07-13 21:38:13 -0700

    Refresh app cache for Block Focus rotation and iPad controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jru-academy-v6';
+const CACHE='jru-academy-v7';
 const CORE=[
   './','./index.html','./manifest.webmanifest','./assets/jru-academy-icon.svg',
   './data/math-lessons.js','./data/reading-lessons.js','./data/quick-challenges.js',
